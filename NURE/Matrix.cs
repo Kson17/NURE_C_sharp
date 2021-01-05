f566fdd7ceb8489f49d7e97a6f3b265f40eecf04
Alexey Katasonov <alexey.kson@gmail.com>
2021-01-05 18:02:18 +0200

    marix class and math

---
+++ NURE/Matrix.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+namespace NURE
+{
+    class Matrix
+    {
+        public int n;
+        public int[,] mass;
+
+
+        public Matrix()
+        {
+            // хардкод для создания 2х2 матрицы
+            this.n = 2;
+            mass = new int[2, 2];
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                return mass[i, j];
+            }
+            set
+            {
+                mass[i, j] = value;
+            }
+        }
+
+
+        public void WriteMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    mass[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+
+
+        public void ReadMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        // Умножение матрицы на число
+        public static Matrix MultiplyByNumber(Matrix a, int ch)
+        {
+            Matrix result = new Matrix();
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < a.n; j++)
+                {
+                    result[i, j] = a[i, j] * ch;
+                }
+            }
+            return result;
+        }
+
+        // Умножение матриц
+        public static Matrix MultiplyTwoMatrices(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix();
+            for (int i = 0; i < a.n; i++)
+                for (int j = 0; j < b.n; j++)
+                    for (int k = 0; k < b.n; k++)
+                        result[i, j] += a[i, k] * b[k, j];
+
+            return result;
+        }
+
+        // Сложение матриц
+        public static Matrix Sum(Matrix a, Matrix b)
+        {
+            Matrix result = new Matrix();
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < b.n; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Сравнение матриц
+        public static bool Equals(Matrix a, Matrix b)
+        {
+            for (int i = 0; i < a.n; i++)
+                for (int j = 0; j < b.n; j++)
+                    if (a.mass[i, j] != b[i, j])
+                        return false;
+            return true;
+        }
+
+        // Детерминант матриц 2х2
+        public double Det2x2()
+        {
+            double det;
+            det = mass[0, 0] * mass[1, 1] - mass[0, 1] * mass[1, 0];
+
+            return det;
+        }
+
+        // Транспонирование матриц
+        public static Matrix Transpose(Matrix a)
+        {
+            Matrix result = new Matrix();
+
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < a.n; j++)
+                {
+                    result[j, i] = a.mass[i, j];
+                }
+            }
+
+            return result;
+        }
+
+
+
+        // перегрузка оператора умножения матриц
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            return Matrix.MultiplyTwoMatrices(a, b);
+        }
+
+        // перегрузка оператора умножения матрицы на число
+        public static Matrix operator *(Matrix a, int b)
+        {
+            return Matrix.MultiplyByNumber(a, b);
+        }
+       
+        // перегрузка оператора сложения
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            return Matrix.Sum(a, b);
+        }
+
+        // перегрузка оператора сравнения
+        public static bool operator ==(Matrix a, Matrix b)
+        {
+            return Matrix.Equals(a, b);
+        }
+
+        public static bool operator !=(Matrix a, Matrix b)
+        {
+            return !Matrix.Equals(a, b);
+        }
+    }
+}
---
+++ NURE/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NURE
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Matrix matrix1 = new Matrix();
+            Matrix matrix2 = new Matrix();
+            Matrix matrix3 = new Matrix();
+
+            Console.WriteLine("Ввод Матрица 1: ");
+            matrix1.WriteMatrix();
+            Console.WriteLine("Ввод Матрица 2: ");
+            matrix2.WriteMatrix();
+
+            Console.WriteLine("Матрица 1: ");
+            matrix1.ReadMatrix();
+            Console.WriteLine();
+            Console.WriteLine("Матрица 2: ");
+            Console.WriteLine();
+            matrix2.ReadMatrix();
+
+            Console.WriteLine("Сложение матриц 1 и 2: ");
+            matrix3 = (matrix1 + matrix2);
+            matrix3.ReadMatrix();
+
+            Console.WriteLine("Умножение матриц 1 и 2: ");
+            matrix3 = (matrix1 * matrix2);
+            matrix3.ReadMatrix();
+
+            Console.WriteLine("Умножение матрицы 1 на число 2: ");
+            matrix3 = (matrix1 * 2);
+            matrix3.ReadMatrix();
+
+            Console.Write("Сравнение матрицы 1 и 2: ");
+            bool result = (matrix1 == matrix2);
+            Console.WriteLine(result);
+
+            Console.Write("Детерминант матрицы 1: ");
+            Console.WriteLine(matrix1.Det2x2());
+
+            Console.WriteLine("Транспонирование матрицы 1: ");
+            matrix3 = Matrix.Transpose(matrix1);
+            matrix3.ReadMatrix();
+
+            Console.ReadKey();
+        }
+    }
+}
